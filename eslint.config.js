0e07c9d179d72fe96e2c04fa095e29c6d2f513cd
sr jaiswal <reeacc1012@gmail.com>
2024-09-15 21:26:13 +0530

    started adding basic redux setup

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import globals from 'globals'
 import react from 'eslint-plugin-react'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 export default [
   { ignores: ['dist'] },
@@ -22,8 +23,10 @@ export default [
       react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+      'simple-import-sort':simpleImportSort,
     },
     rules: {
+      'simple-import-sort/imports':'error',
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Toaster } from 'react-hot-toast'
+import { BrowserRouter } from 'react-router-dom'
+import store from './Redux/store.js'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
+  <Provider store = {store}>
+    <BrowserRouter>
+      <App />
+    <Toaster/>
+
+    </BrowserRouter>
+  </Provider>
   
-    <App />
+    
   ,
 )
